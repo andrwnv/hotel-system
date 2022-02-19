6ceeeec541b7d5d5b9f52dc65fa3bc7525a54cbc
Andrew G <glazynovand@gmail.com>
2022-02-19 18:36:17 +0700

    add basic func for check dates correctness

---
+++ Core/Utils/DayChecks.hs
@@ -0,0 +1,26 @@
+module Core.Utils.DayChecks where
+
+import Data.Time
+import Data.Time.Calendar
+import Data.Dynamic
+
+now :: IO Day
+now = do
+    now <- getCurrentTime
+    let (year, month, day) = toGregorian $ utctDay now
+    let currentDate = (fromGregorian year month day)
+    return currentDate
+
+isCorrectDay :: Day -> IO Bool
+isCorrectDay day = do
+    currentDay :: Day <- now 
+    let result = currentDay <= day
+    return result
+
+isCorrectDatePair :: [Day] -> IO Bool
+isCorrectDatePair dates = do
+    let begin = dates!!0 
+    let end = dates!!1
+    d1 <- isCorrectDay begin
+    d2 <- isCorrectDay end
+    return (length dates == 2 && begin <= end && d1 && d2)
