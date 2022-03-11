e3188251a6165f38e3edf7ac072ebdaa65040396
Andrew G <glazynovand@gmail.com>
2022-03-11 11:04:34 +0700

    entities/room: add rent type

--- entities/Room.hs
+++ entities/Room.hs
@@ -5,14 +5,15 @@ import Tenant
 
 import Data.Time
 
+data Rent = Tuple ([Tenant], [Day]) deriving (Show, Read)
 
 data Room = Room {
-    roomNumber  :: Int,
-    description :: String,
-    price       :: Double,
-    comforts    :: [RoomComfortItem],
-    dayExpenses :: Double,
-
-    busyTime    :: [Day],
-    busyBy      :: [Tenant]
+    roomNumber   :: Int,
+    description  :: String,
+    price        :: Double,
+    comforts     :: [RoomComfortItem],
+    dayExpenses  :: Double,
+    busyTime     :: [Day],
+    busyBy       :: [Tenant],
+    plannedRents :: [Rent]
 } deriving (Show, Read)
