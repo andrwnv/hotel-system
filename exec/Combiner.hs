2e210f77b0c91ec09926c63c9093950058bd2e4a
Andrew G <glazynovand@gmail.com>
2022-03-13 19:41:20 +0700

    exec\combiner: layer for connectin core and ui

---
+++ exec/Combiner.hs
@@ -0,0 +1,55 @@
+{-# LANGUAGE OverloadedStrings, OverloadedLabels, ScopedTypeVariables, LambdaCase, InstanceSigs #-}
+
+module Combiner where
+
+-- Prelude
+import Data.Text (Text, unpack)
+import Data.IORef
+import Control.Monad
+
+-- Gtk
+import qualified GI.Gtk as Gtk
+
+-- Core
+import UserCore
+
+-- Entities
+import PersonBase
+import Tenant
+import Hotel
+
+-- View
+import Extractors
+import qualified ViewID as ID 
+
+createUserHandler :: Gtk.Builder -> IORef Hotel -> IO ()
+createUserHandler uiBuilder hotel = do
+    phoneNumber     <- extractEntryText uiBuilder (ID.create_PhoneNumberFieldID)
+    firstName       <- extractEntryText uiBuilder (ID.create_FirstNameFieldID)
+    lastName        <- extractEntryText uiBuilder (ID.create_LastNameFieldID)
+    birthDay        <- extractDate      uiBuilder (ID.create_birthDayCalendarID)
+    email           <- extractEntryText uiBuilder (ID.create_EmailFieldID)
+
+    hotelCopy <- readIORef hotel
+    let _users = tenants hotelCopy
+
+    let tenantBase = PersonBase (unpack firstName) (unpack lastName) (unpack phoneNumber) birthDay
+    let isUserValid = isValidUser tenantBase
+
+    if isUserValid
+        then do
+            let (users, success) = createUser _users (Tenant tenantBase (unpack email) (-1))
+            case success of
+                True -> do 
+                    let _rooms = rooms hotelCopy
+                    let _history = history hotelCopy
+                    writeIORef hotel (Hotel users _rooms _history)
+                    print $ "[CREATE]: User -> " ++ show tenantBase
+                False -> print $ "[CREATE]: User already exists -> " ++ show tenantBase
+    else print $ "[CREATE]: Invalid user info -> " ++ show tenantBase
+
+
+deleteUserHandler :: Gtk.Builder -> IORef Hotel -> IO()
+deleteUserHandler uiBuilder hotel = do
+    x <- readIORef hotel
+    print x
